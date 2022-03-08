d6b962597b845aa434d7c2419c11fb5139ea9511
Luciferxgkp <76958987+Luciferxgkp@users.noreply.github.com>
2022-03-08 16:28:36 +0530

    Delete Bazzar_Backend directory

--- Bazzar_Backend/src/routes/admin/order.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { requireSignin , adminMiddleware} = require('../../common-middleware');
-const { updateOrder } = require('../../controller/admin/order.admin');
-
-router.post(`/order/update`,requireSignin,adminMiddleware,updateOrder);
-module.exports = router;
