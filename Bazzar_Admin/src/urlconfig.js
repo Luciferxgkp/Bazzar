ecd0fd58045c0ec9cc5c7a271da3b3b026356cf9
Gourav Kumar <76958987+Luciferxgkp@users.noreply.github.com>
2023-05-17 13:36:13 +0530

    updated the admin url

--- Bazzar_Admin/src/urlconfig.js
+++ Bazzar_Admin/src/urlconfig.js
@@ -1,4 +1,4 @@
-export const api = "http://localhost:2000/api";
+export const api = "https://bazzar-backend-5a17.onrender.com/api";
 export const generatePublicUrl = (fileName) => {
-  return `http://localhost:2000/public/${fileName}`;
+  return `https://bazzar-backend-5a17.onrender.com/public/${fileName}`;
 };
